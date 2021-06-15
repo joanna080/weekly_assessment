966de63e7a340513e5e2a39544fac4a0de185557
joanna080 <85246681+joanna080@users.noreply.github.com>
2021-06-15 22:27:43 +0100

    Add files via upload

---
+++ Answer 2.kt
@@ -0,0 +1,23 @@
+fun main(){
+    var j = 1
+    while(j <= 100){
+        println(j)
+        j++
+    }
+    println()
+    println()
+
+    for(c in 1..10){
+       if(c == 4 || c == 5){
+            continue
+        }
+        println(c)
+
+    }
+
+
+}
+
+
+
+    
