ac0934a7d35fb96ac110652647b2ccfcea572e92
joanna080 <85246681+joanna080@users.noreply.github.com>
2021-06-20 20:33:44 +0100

    Add files via upload

---
+++ number 1 answer..kt
@@ -0,0 +1,18 @@
+fun main(){
+  for (numb in 1..100){
+  	if(numb %3==0 && numb%5==0)
+  	{println("FizzBuzz")
+ } else if (numb%5==0){
+ 	println("Buzz")
+ 	}else if (numb%3==0){
+ 		println("Fizz")}
+ 		else{
+ 			println(numb)
+	}
+	}		
+ }
+
+
+    
+
+	
