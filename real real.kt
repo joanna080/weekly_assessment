5f910e70b03fc4d8176616ee85014bda75c2c073
joanna080 <85246681+joanna080@users.noreply.github.com>
2021-07-13 14:33:37 -0700

    Add files via upload

---
+++ real real.kt
@@ -0,0 +1,17 @@
+fun main(){
+	LoveCal(4,4)
+	LoveCal(22,17)
+}
+
+
+fun LoveCal
+ (flower1:Int, flower2:Int){
+ 	
+ var result = if(flower 1 % 2! = flower 2 % 2){
+ 	
+println ("true")
+	}else{
+		
+println("false")
+	}
+    }
